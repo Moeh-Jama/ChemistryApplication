fbd6410dd8832bfbb428e056b030f32cc1ca604c
Mohamed Jama <m.d.jama@hotmail.com>
2017-05-22 13:27:25 +0100

    Combinations of Elements
    able to combine elements and get their resulting Atomic Weight

--- gatherElements.c
+++ gatherElements.c
@@ -1,5 +1,6 @@
 //https://www.reddit.com/r/dailyprogrammer/comments/5seexn/20170206_challenge_302_easy_spelling_with/?utm_content=title&utm_medium=hot&utm_source=reddit&utm_name=dailyprogrammer
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -12,13 +13,40 @@ struct chemistry_Periodic_Elements{
 };
 struct chemistry_Periodic_Elements array[103];
 struct chemistry_Periodic_Elements temp[103];
+int size_Of_Array;
+void mixing();
+void print();
+void createArray();
+void sort_Periodic_Table();
+int iteration =0;
 int main(void){
+	if(iteration==0)
+	{
+		createArray();
+		iteration++;
+	}
 
-	printf("Enter the Symbol of an Element in the periodic table!\n");
-	char element_symbol[20];
-	scanf("%s", element_symbol);
+	printf("Choose one of the following: \n");
+	printf("\t1.Combine two elements.\n");
+	printf("\t2.Print Periodic Table.\n");
+	printf("\t3.Sort Periodic listing.\n");
+	int choice;
+	printf("\nEnter: ");
+	scanf("%d", &choice);
 
-	int size_Of_Array;
+	switch(choice)
+	{
+		case 1: mixing(); main(); break;
+		case 2: print(); main(); break;
+		case 3: sort_Periodic_Table(); main();break;
+		default: printf("Error Invalid Entry\n"); exit(0);
+	}
+
+
+	return 0;
+}
+void createArray()
+{
 
 	FILE *fptr;
 	if((fptr=fopen("elements.txt", "r"))==NULL)
@@ -35,13 +63,43 @@ int main(void){
 		size_Of_Array=i;
 	}
 	fclose(fptr);
+}
+void sort_Periodic_Table()
+{
+	printf("CHoose a sorting method\tTo be Continued.\n");
+}
+void mixing(){
+		printf("\nEnter two Element Symbols to Combine and get the atomic weight of the result.\n");
+	char first_Element[5], second_Element[5];
+	printf("First Element: ");
+	scanf("%s", first_Element);
+	printf("\nSecond Element: ");
+	scanf("%s", second_Element);
 
+	double total_Weight = 0;
+	for (int i = 0; i < size_Of_Array; ++i)
+	{
 
+		int ret1 = strcmp(array[i].Symbol, first_Element);
+		if(ret1==0)
+		{
+			total_Weight +=array[i].Atomic_Weight;
+		}
 
+		int ret2 = strcmp(array[i].Symbol, second_Element);
+		if(ret2==0)
+		{
+			total_Weight +=array[i].Atomic_Weight;
+		}
+		
+	}
+	printf("Combining of: %s and %s\n",first_Element, second_Element);
+	printf("Total weight is: %lf\n", total_Weight);
+}
+void print(){
 	printf("%-15s%-15s\t%-5s\t%s,\t%s\n", "Element","Symbol","Z","Atomic Weight","c");
 	for(int i=01; i<size_Of_Array; i++)
 	{
 		printf("%-15s%-15s\t%d\t%-10.4f\t%.2f\n",array[i].Element, array[i].Symbol, array[i].Z, array[i].Atomic_Weight, array[i].c);
 	}
-	return 0;
 }
