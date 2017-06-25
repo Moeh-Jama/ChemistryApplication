c26e1e9520b76ca4b1c303ab586eb56562ccc06d
Mohamed Jama <m.d.jama@hotmail.com>
2017-06-25 01:52:40 +0100

    Reduced function listings

--- program_core.h
+++ program_core.h
@@ -1,3 +1,9 @@
+/*
+ * controlFile.c
+ *
+ *  Created on: Apr 10, 2017
+ *      Author: Mohamed Jama.
+ */
 
 #ifndef PROGRAM_CORE_H_
 #define PROGRAM_CORE_H_
@@ -13,9 +19,12 @@ struct chemistry_Periodic_Elements{
 	int Z;				//Number of protons.
 	float Atomic_Weight;
 	float c;
-	char *Elect_Confg;
+	char Elect_Confg[25];
 	int Groups;
+	int layer;
+	float mp;
 };
+// Important arrays and variables.
 struct chemistry_Periodic_Elements array[119];
 struct chemistry_Periodic_Elements temp[119];
 int size_Of_Array;
@@ -23,11 +32,11 @@ int size_Of_Array;
 
 
 //Functions.
-void set_Groups();
 void mixing();
 void print();
 void createArray();
-void testEntry();
-void grouping();
+double get_Element_Value();
 void sort_Periodic_Table();
 double getElement(char Symbol[], int subscript);
+double compund_element();
+int get_value_of_subscript(char *element, int l);
